ff6b61db8d3ddc9322a2940319ad3224efe939c3
Maxim-ilan Ganansia <m.gnansia@gmail.com>
2020-11-08 13:22:07 +0200

    upload heroku

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 async function searchNasdaq(inputSearch) {
     const response = await fetch(`https://financialmodelingprep.com/api/v3/search?query=${inputSearch}&limit=10&exchange=NASDAQ&apikey=ed93f3e229380c530b7a0e7663f86b99`)
     const data = await response.json();
