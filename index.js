807dbdf765e88abdf811040cb81bff9e6d342d1d
Maxim-ilan Ganansia <m.gnansia@gmail.com>
2020-11-08 13:15:51 +0200

    some info added

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 async function searchNasdaq(inputSearch) {
     const response = await fetch(`https://financialmodelingprep.com/api/v3/search?query=${inputSearch}&limit=10&exchange=NASDAQ&apikey=ed93f3e229380c530b7a0e7663f86b99`)
     const data = await response.json();
@@ -18,11 +21,11 @@ async function getCompanyProfile(symbol) {
     return data
 }
 
-async function searchNasdaqWithProfile(inputSearch){
+async function searchNasdaqWithProfile(inputSearch) {
     const companies = await searchNasdaq(inputSearch);
-    const fetchCompaniesProfile = companies.map(company =>{
+    const fetchCompaniesProfile = companies.map(company => {
         return getCompanyProfile(company.symbol)
-    }) 
+    })
     const companyProfile = await Promise.all(fetchCompaniesProfile);
     return companyProfile;
 }
